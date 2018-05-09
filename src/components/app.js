c92f179a283304a2e98091eb6c4cb923cd6804f6
jordanhudgens <jordan@devcamp.com>
2018-05-09 16:14:23 -0500

    Basic functional component list addition

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+const JournalList = () => {
+  return "Some journal entries...";
+};
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>DevCamp React Starter</h1>
+        <h1>React, Props, and State Deep Dive</h1>
+
+        <JournalList />
       </div>
     );
   }
